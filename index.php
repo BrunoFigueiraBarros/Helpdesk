95ae3a3adbc0d0639b72b74f269a1a055c59d521
BrunoFigueiraBarros <bruno_figueira59@hotmail.com>
2022-12-08 13:32:36 -0300

    atualização

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head lang="es">
+<head lang="pt-br">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
--- view/MainHead/head.php
+++ view/MainHead/head.php
@@ -1,4 +1,4 @@
-<head lang="es">
+<head lang="pt-br">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
