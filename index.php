6d3555d879bab84a2f2b0b42335e659e2f4f0c8f
BrunoFigueiraBarros <56081466+BrunoFigueiraBarros@users.noreply.github.com>
2023-06-16 22:00:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,6 @@
             </div>
         </div>
     </div>
-
 <script src="public/js/lib/jquery/jquery.min.js"></script>
 <script src="public/js/lib/tether/tether.min.js"></script>
 <script src="public/js/lib/bootstrap/bootstrap.min.js"></script>
